621747762e6290bf368fabfb0f84dc5d921b6f54
vitalii-8D <gotbubu0@gmail.com>
2023-04-16 22:46:17 +0300

    Lesson 5.
    Tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
+  // Matcher
+  const articleShape = expect.objectContaining({
+    id: expect.any(Number),
+    body: expect.any(String),
+    title: expect.any(String),
+    createdAt: expect.any(String),
+    updatedAt: expect.any(String),
+    published: expect.any(Boolean),
+  });
+
+  const articlesData = [
+    {
+      // id: 101,
+      title: 'atitle3',
+      description: 'desc1',
+      body: 'body1',
+      published: true,
+    },
+    {
+      // id: 102,
+      title: 'atitle4',
+      description: 'desc2',
+      body: 'body2',
+      published: false,
+    },
+  ];
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +41,44 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    // Needed to run validation properly
+    useContainer(app.select(AppModule), { fallbackOnErrors: true });
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+    prisma = app.get<PrismaService>(PrismaService);
+
     await app.init();
+
+    await prisma.article.create({
+      data: articlesData[0],
+    });
+    await prisma.article.create({
+      data: articlesData[1],
+    });
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('GET /articles', () => {
+    it('returns a list of published articles', async () => {
+      const { status, body } = await request(app.getHttpServer()).get(
+        '/articles',
+      );
+
+      expect(status).toBe(200);
+      expect(body).toStrictEqual(expect.arrayContaining([articleShape]));
+      expect(body).toHaveLength(1);
+      expect(body[0].published).toBeTruthy();
+    });
+
+    it('returns a list of unpublished articles', async () => {
+      const { status, body } = await request(app.getHttpServer()).get(
+        '/articles/draft',
+      );
+
+      expect(status).toBe(200);
+      expect(body).toStrictEqual(expect.arrayContaining([articleShape]));
+      expect(body).toHaveLength(1);
+      expect(body[0].published).toBeFalsy();
+    });
   });
 });
